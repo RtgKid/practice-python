cdc5b44998cfbb07a9186f1c0d00ae349f764074
Aramais Khachatryan <rtgkid8@gmail.com>
2021-09-27 18:57:11 +0400

    Learn bool type

---
+++ BuiltInClasses/bool.py
@@ -0,0 +1,19 @@
+# True or False
+
+a = True
+b = False
+
+# Numbers
+
+if 0:
+    print("This is never reachable")
+
+if 54:
+    print("This is always true")
+
+
+if "":
+    print("This is never reachable")
+
+if "anystring":
+    print("This is always true")
