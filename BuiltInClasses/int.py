ed6331955fb21191008816d05c51a1188e82cb4f
Aramais Khachatryan <rtgkid8@gmail.com>
2021-09-27 19:14:27 +0400

    Learn int type

---
+++ BuiltInClasses/int.py
@@ -0,0 +1,21 @@
+#Integer numbers with arbitrary magnitude
+
+#Base10
+print(12323)
+
+#Base 2
+print(0b011)
+
+#Base 8
+print(0o52)
+
+#Base 16
+print(0x7f)
+
+i = int()
+print(i)
+
+print(int(3.1) == int(3.99))
+
+print(int("322"))
+print(int("1a", 16))
