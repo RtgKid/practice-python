8c82408def1793fe6da994636646178276692212
Aramais Khachatryan <rtgkid8@gmail.com>
2021-09-27 19:38:31 +0400

    Learn tuples: creating a tuple with 1 element

--- BuiltInClasses/tuple.py
+++ BuiltInClasses/tuple.py
@@ -4,4 +4,11 @@ print(a)
 
 print(a[0])
 
-a[0] = 5 #Tupples are immutable
+#a[0] = 5 #Tupples are immutable
+
+b = (18) #This is just an integer
+print(b)
+
+
+c = (18,) #This is a tuple with 1 element
+print(c)
