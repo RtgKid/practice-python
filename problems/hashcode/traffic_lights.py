2bf062b65fbae07963a1241ae7fd4eb5d5526587
Aramais Khachatryan <akh@hubspot.com>
2022-02-19 23:52:17 +0400

    Get all sorted by car count

--- problems/hashcode/traffic_lights.py
+++ problems/hashcode/traffic_lights.py
@@ -15,7 +15,6 @@
 
 # Car -> sequence of streets
 
-from sqlite3 import InternalError
 import numpy as np
 import pandas as pd
 
@@ -39,7 +38,7 @@ def read_file(path):
         return int(D), int(I), int(S), int(V), int(F), dfstreets, dfpaths
     
 
-D, I, S, V, F, dfstreets, dfpaths = read_file('qualification_round_2021.in/a.txt')
+D, I, S, V, F, dfstreets, dfpaths = read_file('qualification_round_2021.in/d.txt')
 # print("D :", D)
 # print("I :", I)
 # print("S :", S)
@@ -55,13 +54,30 @@ def sort_streets_by_busiest(dfpaths):
     d = {}
 
     for path in dfpaths["streets"]:
-        for ss in path:
+        for i in range(len(path) - 1):
+            ss = path[i]
             if ss in d:
                 d[ss] = d[ss] + 1
             else:
                 d[ss] = 1
 
+        # for ss in path:
+        #     if ss in d:
+        #         d[ss] = d[ss] + 1
+        #     else:
+        #         d[ss] = 1
+
     return dict(sorted(d.items(), key=lambda item: item[1], reverse=True))      
+
+def start_car_count(dfpaths):
+    d = {}
+
+    for path in dfpaths["streets"]:
+        if path[0] in d:
+            d[path[0]] = d[path[0]] + 1
+        else:
+            d[path[0]] = 1
+    return d        
 
 def in_streets_of_inter(dfstreets):
     d = {}
@@ -79,17 +95,67 @@ def in_streets_of_inter(dfstreets):
 def main():
     busy_streets = sort_streets_by_busiest(dfpaths)
     intersections = in_streets_of_inter(dfstreets)
+    car_count = start_car_count(dfpaths)
     
-    print(busy_streets)
+    # print(dfstreets)
+    # print(dfpaths)
+    # print(busy_streets)
     print(I)
     for interId in intersections:
         print(interId)
-        print(len(intersections[interId]))
-        for ss in intersections[interId]:
-            print(str(ss) + " " + str(busy_streets[ss]))
-        
+        streets = sorted(intersections[interId], key = lambda x: car_count.get(x, 0),reverse=True)
+
+
+        #print(streets)
+        #print(len(intersections[interId]))
+        print(len(streets))
+        #for ss in intersections[interId]:
+        for ss in streets:
+            if ss in busy_streets:
+                print(str(ss) + " " + str(busy_streets[ss]))
+            else:
+                print(str(ss) + " " + str(1))    
+                    
     
         
+def write_to_file():
+    busy_streets = sort_streets_by_busiest(dfpaths)
+    intersections = in_streets_of_inter(dfstreets)
+    car_count = start_car_count(dfpaths)
+    
+    #print(busy_streets)
+    file = open("qualification_round_2021.in/d_ans_car_count.txt", "w")
+    #print(I)
+    file.write(str(I) + "\n")
+    for interId in intersections:
+        #print(interId)
+        file.write(str(interId) + "\n")
+        streets = sorted(intersections[interId], key = lambda x: car_count.get(x, 0),reverse=True)
+        #print(len(intersections[interId]))
+        file.write(str(len(streets)) + "\n")
+
+        iter_strs = []
+        for ss in streets:
+            if ss in busy_streets:
+                iter_strs.append(busy_streets[ss])
+        w_streets = [str(i / min(iter_strs)) for i in iter_strs]        
+
+        for ss in streets:
+            i = 0
+            if ss in busy_streets:
+                #print(str(ss) + " " + str(busy_streets[ss]))
+                file.write(str(ss) + " " + str(w_streets[i]) + "\n")
+            else:
+                file.write(str(ss) + " " + "1" + "\n")
+            i = i + 1    
+                    
+    file.close()            
+
+
+write_to_file()
+
+from scoring import scoring
+print(scoring(input_file='qualification_round_2021.in/d.txt',output_file='qualification_round_2021.in/d_ans_car_count.txt'))
 
     
-main()    
+  
