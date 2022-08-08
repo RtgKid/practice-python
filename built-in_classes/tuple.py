ae1e3b0f7c8e555aa65f2d70cea22a421f737e9a
Aramais Khachatryan <akh@hubspot.com>
2022-08-08 21:54:48 +0400

    Revisit the tuples

--- built-in_classes/tuple.py
+++ built-in_classes/tuple.py
@@ -1,3 +1,6 @@
+from tokenize import maybe
+
+
 a = (16, 13, 5)
 
 print(a)
@@ -12,3 +15,26 @@ print(b)
 
 c = (18,) #This is a tuple with 1 element
 print(c)
+
+b = "banana", "apple"
+print(b)
+print(type(b))
+
+c = ["banana", "apple"]
+tup = tuple(c)
+print(tup)
+
+if "banana" in tup:
+    print("yes")
+
+print(len(tup))    
+print(tup.count("banana"))
+print(tup.index("banana"))
+
+print(list(tup))
+
+
+my_tuple = (0, 1, 3, 4, 5, 6)
+i1, *i2, i3 = my_tuple
+
+print(i1, i2, i3)
