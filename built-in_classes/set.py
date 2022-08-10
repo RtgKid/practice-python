3b569d6725296f8996bacadb8544096c8e620b9c
Aramais Khachatryan <akh@hubspot.com>
2022-08-10 12:05:18 +0400

    Revisit the sets

--- built-in_classes/set.py
+++ built-in_classes/set.py
@@ -6,3 +6,41 @@ print(type(b))
 
 c = set()
 print(c)
+
+myset = set("Hello")
+print(myset)
+
+myset = set([1, 2, 3, 1, 2])
+print(myset)
+
+myset.add(5)
+print(myset)
+
+#Remove without key error
+myset.discard(6)
+print(myset)
+
+#Union
+odds = {1, 3, 5, 7, 9}
+evens = {2, 4, 6, 8, 10}
+primes = {2, 3, 5, 7}
+
+u = odds.union(evens)
+print(u)
+
+i = odds.intersection(evens)
+print(i)
+
+i = odds.intersection(primes)
+print(i)
+
+d = primes.difference(evens)
+print(d)
+
+sd = primes.symmetric_difference(evens)
+print(sd)
+
+#subset and superset
+print(evens.issubset(odds))
+print(evens.issuperset(odds))
+print(evens.isdisjoint(odds))
